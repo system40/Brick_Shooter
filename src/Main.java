79b463218c540c8861f36112ed597db532374b3b
system40 <154955596+system40@users.noreply.github.com>
2023-12-27 09:13:50 +0000

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,13 @@ import java.awt.Color;
 import javax.swing.JFrame;
 
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		JFrame obj=new JFrame();
 		Gameplay gamePlay = new Gameplay();
 		
 		obj.setBounds(10, 10, 700, 600);
-		obj.setTitle("Breakout Ball");		
+		obj.setTitle("Brick Breaker");		
 		obj.setResizable(false);
 		obj.setVisible(true);
 		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
